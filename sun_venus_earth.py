dc316b6bee221e06964717a98f94dab325ef4fdd
Saeid Mahmoudzadeh <saeid@145.118.EARLY-REGISTRATION.of.SURFnet.invalid>
2019-09-26 17:18:16 +0200

    using BHtree

--- sun_venus_earth.py
+++ sun_venus_earth.py
@@ -1,7 +1,5 @@
 ###BOOKLISTSTART1###
-from amuse.lab import Particles, units 
-
-print(type(units.RSun))
+from amuse.lab import Particles, units
 
 v1 = 0.201
 v2 = 0.243
@@ -9,22 +7,22 @@ v2 = 0.243
 def sun_venus_and_earth():
     particles = Particles(3)
     sun = particles[0]
-    sun.mass = 1.0 | units.MSun
+    sun.mass = 1.0 | units.kg
     sun.radius = 1e-5 | units.m
-    sun.position = (-1, 0, 0) | units.AU
-    sun.velocity = (v1, v2, 0) | units.kms
+    sun.position = (-1, 0, 0) | units.m
+    sun.velocity = (v1, v2, 0) | units.ms
 
     venus = particles[1]
-    venus.mass = 1.0 | units.MSun
+    venus.mass = 1.0 | units.kg
     venus.radius = 1e-5 | units.m
-    venus.position = (1, 0, 0) | units.AU
-    venus.velocity = (v1, v2, 0) | units.kms
+    venus.position = (1, 0, 0) | units.m
+    venus.velocity = (v1, v2, 0) | units.ms
 
     earth = particles[2]
-    earth.mass = 0.5 | units.MSun
+    earth.mass = 0.5 | units.kg
     earth.radius = 1e-5 | units.m
-    earth.position = (0, 0, 0) | units.AU
-    earth.velocity = (-4 * v1, -4 * v2, 0) | units.kms
+    earth.position = (0, 0, 0) | units.m
+    earth.velocity = (-4 * v1, -4 * v2, 0) | units.ms
 
     particles.move_to_center()
     return particles
@@ -34,30 +32,28 @@ def sun_venus_and_earth():
 def integrate_solar_system(particles, end_time):
     from amuse.lab import Huayno, nbody_system
     #print(particles[1].position())
-    convert_nbody = nbody_system.nbody_to_si(1.0 | units.MSun,
-                                             1.0 | units.AU)
-    print(convert_nbody)
+    #convert_nbody = nbody_system.nbody_to_si(1.0 | units.MSun,
+    #                                         1.0 | units.AU)
+    #print(convert_nbody)
 
-    gravity = Huayno(convert_nbody)
+    gravity = BHtree()
     gravity.particles.add_particles(particles)
     venus = gravity.particles[1]
     earth = gravity.particles[2]
     sun = gravity.particles[0]
     
     
-    x_earth = [] | units.AU
-    y_earth = [] | units.AU
-    x_venus = [] | units.AU
-    y_venus = [] | units.AU
-    x_sun = [] | units.AU
-    y_sun = [] | units.AU
-    
-    #print('Error is here')
-    #print(gravity.model_time + 1.|units.s)
+    x_earth = [] | units.m
+    y_earth = [] | units.m
+    x_venus = [] | units.m
+    y_venus = [] | units.m
+    x_sun = [] | units.m
+    y_sun = [] | units.m
+
 
     while gravity.model_time < end_time:
         
-        gravity.evolve_model(gravity.model_time + (1 | units.day))
+        gravity.evolve_model(gravity.model_time + (1 | units.s))
         x_earth.append(earth.x)
         y_earth.append(earth.y)
         x_venus.append(venus.x)
@@ -79,8 +75,8 @@ def plot_track(xe,ye,xv,yv,xs,ys, output_filename):
     ax.minorticks_on() 
     ax.locator_params(nbins=3)
 
-    x_label = 'x [au]'
-    y_label = 'y [au]'
+    x_label = 'x [m]'
+    y_label = 'y [m]'
     pyplot.xlabel(x_label)
     pyplot.ylabel(y_label)
 
@@ -110,6 +106,6 @@ if __name__ in ('__main__','__plot__'):
     o, arguments  = new_option_parser().parse_args()
 
     particles = sun_venus_and_earth()
-    xe,ye, xv,yv, xs,ys = integrate_solar_system(particles, 2 | units.yr)
+    xe,ye, xv,yv, xs,ys = integrate_solar_system(particles, 200 | units.s)
     plot_track(xe, ye, xv, yv, xs, ys, o.output_filename)
     
