aa42dd580ddd13b159578925aba6699aadf3ba6c
Saeid Mahmoudzadeh <s.mahmoudzadeh@pczaal2.strw.leidenuniv.nl>
2019-10-02 11:33:15 +0200

    neater code w. boundry condiion

--- 3_body_braid_boundry_condition.py
+++ 3_body_braid_boundry_condition.py
@@ -1,20 +1,23 @@
-###BOOKLISTSTART1###
 from amuse.lab import Particles, units
-from amuse.lab import SmallN, Huayno, Hermite, nbody_system
+from amuse.lab import Hermite, nbody_system
 import numpy as np
 
+#Initial Condition
 v1_init = 0.9911981217
 v2_init = 0.7119472124
 m3 = 4
-init = np.array([-1,0,v1_init,v2_init])
-P = 17.6507807837
-T = P*1 | nbody_system.time
+Cycle = 1
+lower_boundry = 1e-1
+upper_boundry = 25
+#P = 17.6507807837 # uncomment, if you want to use time boundry
+#T = P * 1 | nbody_system.time # uncomment, if you want to use time boundry
 
-def sigma(x1,x2,v1,v2):
+
+def sigma(x1,x2, v1,v2): #Calculates the return proximity function at any point
+    init = np.array([-1, 0, v1_init, v2_init])
     current = np.array([x1, x2, v1, v2])
     return np.sqrt(np.sum((current - init) ** 2))    
     
-
 def body_init():
     particles = Particles(3)
     body0 = particles[0]
@@ -34,10 +37,8 @@ def body_init():
     
     particles.move_to_center()
     return particles
-###BOOKLISTSTOP1###
 
-###BOOKLISTSTART2###
-def integrate_solar_system(particles, end_time):
+def integrate_3body_system(particles):#, end_time):
     from amuse.units import constants, units
     import matplotlib.pyplot as plt
    
@@ -53,7 +54,6 @@ def integrate_solar_system(particles, end_time):
     y_body1 = [] | nbody_system.length
     x_body2 = [] | nbody_system.length
     y_body2 = [] | nbody_system.length
-
     vx_body0 = [] | nbody_system.speed
     vy_body0 = [] | nbody_system.speed
 
@@ -63,28 +63,26 @@ def integrate_solar_system(particles, end_time):
     y_body1.append(body1.y)
     x_body2.append(body2.x)
     y_body2.append(body2.y)    
-
     vx_body0.append(body0.vx)
     vy_body0.append(body0.vy)
     
-    
-    
+    # Plotting the return proximity funcion
     fig = plt.figure(figsize=(8,16))
     ax1 = fig.add_subplot(211, xlabel = 'Integraion time step', ylabel = 'Return proximity function', 
 				title='Deviation from the initial point after each period')
     ax2 = fig.add_subplot(212, xlabel = 'Intergration time step', ylabel = 'Return proximity function',
 				title='Return proximity function per time step')
-
+	
     sig = sigma(np.array(body0.x.value_in(nbody_system.length)),
 	        np.array(body0.y.value_in(nbody_system.length)),
 	        np.array(body0.vx.value_in(nbody_system.speed)),
 	        np.array(body0.vy.value_in(nbody_system.speed)))
-    Cycle = 0
+    c = 0 # cycle
     sig_array = []
     time_step = 0
     sig_O_x = [0]
     sig_O_y = [sig]
-    while (Cycle < 4 and sig < 200): #gravity.model_time < end_time:
+    while (c < Cycle and sig < upper_boundry): # gravity.model_time < end_time:
 	
 	gravity.evolve_model(gravity.model_time + (0.005 | nbody_system.time))
 	x_body0.append(body0.x)
@@ -101,22 +99,21 @@ def integrate_solar_system(particles, end_time):
 	        np.array(body0.y.value_in(nbody_system.length)),
 	        np.array(body0.vx.value_in(nbody_system.speed)),
 	        np.array(body0.vy.value_in(nbody_system.speed)))
+	#print(sig)
 	sig_array.append(sig)
 	if time_step > 2: 
 		slope = (sig_array[-2] - sig_array[-3]) * (sig_array[-1] - sig_array[-2])
 	else: slope = 1
 		
-	if (sig < 10e-2) and (slope < 0):
-		Cycle += 1
+	if (sig < lower_boundry) and (slope < 0):
+		c += 1
 		print(x_body0.value_in(nbody_system.length)[-1], y_body0.value_in(nbody_system.length)[-1],
 		vx_body0.value_in(nbody_system.speed)[-1], vy_body0.value_in(nbody_system.speed)[-1])
 		print(sig)
 		sig_O_x.append(time_step)
 		sig_O_y.append(sig)
 		
-	#plt.scatter(time_step, sig, color='b')
 	time_step += 1
-    #print(gravity.model_time)
     gravity.stop()
     ax1.plot(sig_O_x, sig_O_y, color = 'k')
 
@@ -127,9 +124,6 @@ def integrate_solar_system(particles, end_time):
     plt.show()
     return x_body0, y_body0, x_body1, y_body1, x_body2, y_body2
     
-###BOOKLISTSTOP2###
-
-###BOOKLISTSTART3###
 def plot_track(x0,y0,x1,y1,x2,y2, output_filename):
 
     from matplotlib import pyplot
@@ -145,10 +139,6 @@ def plot_track(x0,y0,x1,y1,x2,y2, output_filename):
     pyplot.xlabel(x_label)
     pyplot.ylabel(y_label)
     
-    #print('Printing x-values')
-    #print(np.array(x0.value_in(nbody_system.length)))
-    #print(len(x0.value_in(nbody_system.length)))
-    #print(x0.value_in(nbody_system.length).shape)
     plot.plot(x0.value_in(nbody_system.length), y0.value_in(nbody_system.length), color = 'b')
     plot.plot(x1.value_in(nbody_system.length), y1.value_in(nbody_system.length), color = 'r')
     plot.plot(x2.value_in(nbody_system.length), y2.value_in(nbody_system.length), color = 'k')
@@ -160,7 +150,6 @@ def plot_track(x0,y0,x1,y1,x2,y2, output_filename):
     pyplot.savefig(save_file)
     print '\nSaved figure in file', save_file,'\n'
     pyplot.show()
-###BOOKLISTSTOP3###
 
 def new_option_parser():
     from amuse.units.optparse import OptionParser
@@ -174,6 +163,6 @@ if __name__ in ('__main__','__plot__'):
     o, arguments  = new_option_parser().parse_args()
 
     particles = body_init()
-    x0,y0, x1,y1, x2,y2= integrate_solar_system(particles, T)
+    x0,y0, x1,y1, x2,y2= integrate_3body_system(particles)#, T)
     plot_track(x0, y0, x1, y1, x2, y2, o.output_filename)
     
