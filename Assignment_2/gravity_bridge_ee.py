29bbe8c9998ffd16d3a83dc0d82daae38d8cd2f5
Yuan Chen <ychen@pczaal2.strw.leidenuniv.nl>
2019-10-23 11:15:19 +0200

    final changes

--- Assignment_2/gravity_bridge_ee.py
+++ Assignment_2/gravity_bridge_ee.py
@@ -9,7 +9,6 @@ from amuse.units import quantities
 from amuse.units import constants
 from amuse.units import nbody_system
 from amuse.ext.bridge import bridge
-#from amuse.couple import bridge
 from amuse.community.ph4.interface import ph4
 from amuse.community.bhtree.interface import BHTree
 from amuse.lab import new_plummer_model
@@ -23,29 +22,26 @@ def cluster_init(N,mMin,mMax,alpha,mCluster,rCluster):
     cluster = new_plummer_model(N,convert_nbody=converter)
     mZAMS = new_powerlaw_mass_distribution(N,mMin,mMax,alpha)
     cluster.mass = mZAMS
-    #print(cluster)  
     cluster.scale_to_standard(converter)
-    #print(cluster)
     return cluster
 
 
 def evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster):
 
     print 'Start evolving the cluster with mCut =', mCut
+    # timing
     t1 = time()
 
     converter=nbody_system.nbody_to_si(mCluster,rCluster)
-    # Splitting particle according to mass_cut
+
+    # split particle according to mass_cut
     low_mass_stars = cluster.select(lambda m: m < mCut,["mass"])
     high_mass_stars = cluster.select(lambda m: m >= mCut,["mass"])
-    
-    
-    # Sanity checks    
     print 'Number of low-mass stars:', low_mass_stars.__len__()
     print 'Number of high-mass stars:', high_mass_stars.__len__()
-    #plot_cluster(low_mass_stars, high_mass_stars)
+
     
-    # Making models and assigning particles    
+    # make models and assigning particles    
     code_tree = BHTree(converter)
     code_direct = ph4(converter)
     code_tree.particles.add_particles(low_mass_stars)
@@ -53,16 +49,16 @@ def evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster):
     channel_from_code_tree = code_tree.particles.new_channel_to(low_mass_stars)
     channel_from_code_direct = code_direct.particles.new_channel_to(high_mass_stars)    
     
-    # Making bridge
+    # make bridge
     combined_gravity = bridge()
     combined_gravity.add_system(code_tree,(code_direct,))
     combined_gravity.add_system(code_direct,(code_tree,))
     combined_gravity.timestep = dt
 
-    # Saving the initial total energy
+    # the initial total energy
     E0 = combined_gravity.potential_energy + combined_gravity.kinetic_energy
                      
-    # Evolving the model
+    # evolve the model
     times = quantities.arange(0.|units.Myr, tend+dt, dt)
     n_step = len(times)
     dE = np.empty(n_step)
@@ -76,11 +72,11 @@ def evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster):
 
         combined_gravity.evolve_model(t, timestep=dt)
 
-	U = cluster.potential_energy()
-	T = cluster.kinetic_energy()
-	Et = U + T
-	dE[i] = np.abs((E0-Et)/E0)
-	Qs[i] = -T / U
+        U = cluster.potential_energy()
+        T = cluster.kinetic_energy()
+        Et = U + T
+        dE[i] = np.abs((E0-Et)/E0)
+        Qs[i] = -T / U
 
     code_tree.stop()
     code_direct.stop()
@@ -91,9 +87,9 @@ def evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster):
     with open(output_filename, 'w'):
         np.savetxt(output_filename, np.append(dE,Qs).reshape(2,n_step).transpose())
     print 'Saved the data of energy error and virial ratios in', output_filename
-    # Plotting results
+
+    # plot energy
     plot_energy_error(times.value_in(units.Myr), dE, Qs, mCut)
-    #plot_cluster(low_mass_stars, high_mass_stars)
     return dE[-1], Qs[-1], t2-t1
 
 
@@ -102,49 +98,24 @@ def plot_energy_error(times, dE, Qs, mCut):
     ax = fig_ee.add_subplot(111)
     ax.plot(times, dE, '-o', label='dE/E0')
     ax.plot(times, Qs, '-o', label='Q = -T/U')
-    #ax.set_yscale('log')
-    ax.set_xlabel('Time (Myr)')
+    ax.set_xlabel('Time [Myr]')
     ax.set_ylabel('Relative Energy Error')
     ax.legend()
     #plt.show()
     fig_ee.savefig('energy_error_%.1f.jpg'%mCut.value_in(units.MSun))
-    
-
-def plot_cluster(low_mass, high_mass):
-    
-    m_low = low_mass.mass.value_in(units.MSun)
-    x_low = low_mass.x.value_in(units.parsec)
-    y_low = low_mass.y.value_in(units.parsec)
-    z_low = low_mass.z.value_in(units.parsec)
-    
-    m_high = high_mass.mass.value_in(units.MSun)
-    x_high = high_mass.x.value_in(units.parsec)
-    y_high = high_mass.y.value_in(units.parsec)
-    z_high = high_mass.z.value_in(units.parsec)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111,projection='3d')
-    ax.scatter(x_low,y_low,z_low, c='b',zorder=0)
-    ax.scatter(x_high,y_high,z_high, c='r',zorder=1)    
-    plt.show()
-    
-    masses = np.append(m_high,m_low)
-    plt.hist(masses,range=[0.1,100],bins=100)
-    plt.yscale('log')    
-    plt.show()
-
+   
 
 if __name__ == "__main__":
 
     
-    # Determining number of particles for which the code must be run
+    # determine the number of particles for which the code must be run
     if len(sys.argv) > 1:
         N = int(sys.argv[1])
     else:
         print('N-particles not given, setting N = 10000...')
         N = 10000
 
-    # Setting parameters for cluster
+    # set parameters for cluster
     mMin = 0.1|units.MSun
     mMax = 100|units.MSun
     alpha = -2.35
@@ -152,12 +123,11 @@ if __name__ == "__main__":
     rCluster = 3|units.parsec    
 
     # Setting parameters for evolving the model
-    #mCut_list = np.array([10.0, 8.0, 6.0, 4.0, 2.0, 1.0, 0.8, 0.7, 0.6, 0.5]) | units.MSun
-    #mCut_list = np.array([0.4, 0.5, 0.6, 0.8, 1.0, 2.0, 3.0, 4.0, 6.0, 8.0, 10.0]) |units.MSun
-    #mCut_list = np.array([0.5, 0.6, 0.7]) |units.MSun
-    #mCut_list = np.array([0.8, 0.9, 1.0, 2.0]) |units.MSun
-    mCut_list = np.array([9.0]) |units.MSun
-    mCut = 10|units.MSun
+    mCut_list = np.array([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]) |units.MSun
+    #mCut_low = np.array([0.3, 0.4, 0.5])
+    #mCut_mid = np.array([0.6, 0.7, 0.8, 0.9, 1.0, 2.0]) |units.MSun
+    #mCut_high = np.array([3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]) |units.MSun
+    #mCut_list = [10|units.MSun]
     dt = 0.1 |units.Myr
     tend = 10 |units.Myr
 
@@ -174,22 +144,13 @@ if __name__ == "__main__":
     for i, mCut in enumerate(mCut_list):
         # set a random seed
         np.random.seed(1)
-	cluster = cluster0.copy()
+        cluster = cluster0.copy()
         EE[i], Q[i], wc_time[i] = evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster)
 
-    output_filename = 'ee_q_t_9.txt'
+    output_filename = 'ee_q_t.txt'
     with open(output_filename, 'w'):
         np.savetxt(output_filename, np.append(np.append(EE,Q),wc_time).reshape(3, n_mCut).transpose())
     print 'Saved the data of energy error, virial ratios, and wall-clock time in', output_filename
-
-    #fig = plt.figure(figsize=(6,10))
-    #ax_ee = fig.add_subplot(211)
-    #ax_t = fig.add_subplot(212)
-    #ax_ee.plot(mCut_list.value_in(units.MSun), EE)
-    #ax_ee.plot(mCut_list.value_in(units.MSun), Q)
-    #ax_t.plot(mCut_list.value_in(units.MSun), wc_time)
-    #fig.show()
-    
             
 
 
