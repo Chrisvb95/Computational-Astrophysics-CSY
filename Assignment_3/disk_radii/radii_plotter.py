a6b429e0f106643f0446ad5c63c07f9193fa0ddf
Christiaan Buchem <vbuchem@pczaal6.strw.leidenuniv.nl>
2019-10-30 17:30:07 +0100

    Added all code and data for question 1

---
+++ Assignment_3/disk_radii/radii_plotter.py
@@ -0,0 +1,28 @@
+import pickle as pkl
+import numpy as np
+from matplotlib import pyplot as plt
+from amuse.units import units
+
+
+if __name__ == "__main__":
+
+    radii_dir = 'disk_radii/'   
+
+    N500 = pkl.load(open(radii_dir+'lr9_N500.p','rb'))
+    N1000 = pkl.load(open(radii_dir+'lr9_N1000.p','rb'))
+    N2000 = pkl.load(open(radii_dir+'lr9_N2000.p','rb'))
+    #N4000 = pkl.load(open(radii_dir+'lr9_N4000.p','rb'))
+
+    t_end = 1000 | units.yr
+    n_steps = 1000
+    dt = t_end/float(n_steps)
+    t = np.arange(0,(t_end+dt).value_in(units.yr),dt.value_in(units.yr))    
+
+    plt.plot(t,N500,label='N = 500')
+    plt.plot(t,N1000,label='N = 1000')
+    plt.plot(t,N2000,label='N = 2000')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Disk size (AU)')
+    plt.legend()
+    plt.show()
+
