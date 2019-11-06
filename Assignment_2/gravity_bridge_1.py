Cleaned up code and fixed accretion calculation

Assignment_2/gravity_bridge_1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import sys
 import pickle as pkl
-import time
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from amuse.units import units
@@ -26,8 +25,11 @@ def cluster_init(N,mMin,mMax,alpha,mCluster,rCluster):
     
     return cluster
 
-def evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster,dump=False):
-    
+def evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster,dump=False,dump_dir='data_dump'):
+
+    if dump:
+        print('Will be saving data in:'+dump_dir)    
+
     converter=nbody_system.nbody_to_si(mCluster,rCluster)
     # Splitting particle according to mass_cut
     low_mass_stars = cluster.select(lambda m: m < mCut,["mass"])
@@ -54,8 +56,6 @@ def evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster,dump=False):
     # Making first snapshot    
     #plot_cluster(low_mass_stars,high_mass_stars,'t=0',save=True)    
 
-    start_time = time.time()
-
     # Evolving the model
     times = quantities.arange(0|units.Myr, tend, dt)
     mCut_str = str(mCut.value_in(units.MSun))
@@ -70,14 +70,11 @@ def evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster,dump=False):
         
         if dump:
             pkl.dump(low_mass_stars, 
-                     open('data_dump/t'+time+'_m'+mCut_str+'_low_mass.p',
+                     open(dump_dir+'/t'+time+'_m'+mCut_str+'_low_mass.p',
                      'wb'))
             pkl.dump(high_mass_stars, 
-                     open('data_dump/t'+time+'_m'+mCut_str+'_high_mass.p',
-                     'wb'))
-
-    elapsed_time = time.time()-start_time
-    print('Total runtime of simulation:',elapsed_time)    
+                     open(dump_dir+'/t'+time+'_m'+mCut_str+'_high_mass.p',
+                     'wb')) 
             
     code_tree.stop()
     code_direct.stop()    
@@ -118,7 +115,7 @@ def plot_cluster(low_mass, high_mass, title, save=False):
     
  
 if __name__ == "__main__":
-    
+    import time
     # Determining number of particles for which the code must be run
     if len(sys.argv) > 1:
         N = int(sys.argv[1])
@@ -137,10 +134,15 @@ if __name__ == "__main__":
     mCut = 0|units.MSun
     dt = 0.1 |units.Myr
     tend = 10 |units.Myr
+    
+    np.random.seed(1) #Ensuring that we use the same seed for every cluster init
+    cluster = cluster_init(N,mMin,mMax,alpha,mCluster,rCluster)  
 
-    cluster = cluster_init(N,mMin,mMax,alpha,mCluster,rCluster)
-    evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster,dump=True)    
-
+    start_time = time.time()
+    evolve_cluster(cluster,mCut,dt,tend,mCluster,rCluster,dump=True,dump_dir='data_dump')    
+    elapsed_time = time.time()-start_time
+    print('Total runtime of simulation:',elapsed_time) 
+    
     #plot_cluster(cluster)
 
     

Assignment_3/disk_planet_chris.py
@@ -39,14 +39,12 @@ def plot_map(sph, Sun_and_Jupiter, title, N=100, L=1, show=True):
     Sx = Sun_and_Jupiter[0].x.value_in(units.AU)
     Sy = Sun_and_Jupiter[0].y.value_in(units.AU)
     Sz = Sun_and_Jupiter[0].z.value_in(units.AU)
-    #Sr = Sun_and_Jupiter[0].radius.value_in(units.AU)
     Sr = 1    
     sun = Circle((Sx,Sy),Sr,color='y')
 
     Jx = Sun_and_Jupiter[1].x.value_in(units.AU)
     Jy = Sun_and_Jupiter[1].y.value_in(units.AU)
     Jz = Sun_and_Jupiter[1].z.value_in(units.AU)
-    #Jr = Sun_and_Jupiter[1].radius.value_in(units.AU)
     Jr = 0.5     
     jup = Circle((Jx,Jy),Jr,color='orange')
 
@@ -56,7 +54,7 @@ def plot_map(sph, Sun_and_Jupiter, title, N=100, L=1, show=True):
 
     fig,ax = plt.subplots(1,figsize=(8, 8))    
     ax.imshow(np.log10(1.e-5 + rho.value_in(units.amu / units.cm**3)),
-                  extent=[-L / 2, L / 2, -L / 2, L / 2], vmin=10, vmax=15)
+                  extent=[-L / 2, L / 2, -L / 2, L / 2], vmin=10, vmax=15,origin='lower')
     ax.add_patch(sun)
     ax.add_patch(jup)
     plt.title(title)
@@ -74,16 +72,12 @@ def return_L9_radius(disk,Mstar,Rmin):
     lr,mr = disk.LagrangianRadii(converter)
     return lr[7].value_in(units.AU)
 
+def Hill_radius(a, e, Sun_Jupiter):
+    return a * (1-e) * ((Sun_Jupiter[1].mass / Sun_Jupiter[0].mass) / 3.) ** (1. / 3.)
 
 def hydro_sink_particles(sink, gas):
+
     removed_particles = Particles()
-    #for s in sinks:
-    #    xs, ys, zs = [s.x, s.y, s.z]
-    #    radius_squared = s.radius**2
-    #    insink = gas.select_array(lambda x,y,z: (x-xs)**2+(y-ys)**2+(z-zs)**2 < radius_squared,['x','y','z'])  
-    #    if len(insink)==0:
-    #        return insink
-    
     xs, ys, zs = sink.x, sink.y, sink.z
     radius_squared = sink.radius**2
     insink = gas.select_array(lambda x,y,z: (x-xs)**2+(y-ys)**2+(z-zs)**2 < radius_squared,['x','y','z'])  
@@ -96,6 +90,7 @@ def hydro_sink_particles(sink, gas):
     sink.position = (cm+insink.center_of_mass()*insink.total_mass())/sink.mass
     sink.velocity = (p+insink.total_momentum())/sink.mass
     removed_particles.add_particles(insink)
+    gas.remove_particles(insink)
     
     return removed_particles
 
@@ -132,7 +127,6 @@ def init_sink_particle(m, r, position, velocity):
 def gravity_hydro_bridge(gravity, hydro, sink, local_particles, Rmin, t_end=1000.|units.yr, dt=10.|units.yr):
 
     Sun_and_Jupiter, disk_gas = local_particles
-    #Mstar = gravity.particles[1].mass.in_(units.MSun)
     Mstar = 1.0 | units.MSun
 
     print 'Bridging...'
@@ -148,24 +142,15 @@ def gravity_hydro_bridge(gravity, hydro, sink, local_particles, Rmin, t_end=1000
     channel_to_grav = Sun_and_Jupiter.new_channel_to(gravity.particles)
     channel_to_hydro = disk_gas.new_channel_to(hydro.gas_particles)
 
-    # Sanity checks:
-    print('Sanity checks:')
-    print('Sun coordinates (AU)',gravity.particles[0].x.value_in(units.AU),
-                                 gravity.particles[0].y.value_in(units.AU),
-                                 gravity.particles[0].z.value_in(units.AU))
-    #print('Jupiter coordinates (AU)',gravity.particles[1].x.value_in(units.AU),
-    #                                 gravity.particles[1].y.value_in(units.AU),
-    #                                 gravity.particles[1].z.value_in(units.AU))
-    print('Disk particle map saved to: initial_check_disk.png')    
-    plot_map(hydro,Sun_and_Jupiter,'initial_check_disk.png',show=True)
-
-    a_Jup = list()
-    e_Jup = list()
-    disk_size = list()
+    # Preparing lists for data-recording
+    a_Jup = []
+    e_Jup = []
+    disk_size = []
     accreted_mass = []
-    sink0_mass = 0 | units.MSun
+    accreted_mass.append((sink.mass).value_in(units.MJupiter)[0])
+    ink0_mass = 0 | units.MJupiter
 
-    # start evolotuion
+    # Start evolution
     print 'Start evolving...'
     times = quantities.arange(0.|units.yr, t_end+1*dt, dt)
     model_time = 0.0 | units.yr
@@ -179,28 +164,26 @@ def gravity_hydro_bridge(gravity, hydro, sink, local_particles, Rmin, t_end=1000
         a_Jup.append(a)
         e_Jup.append(e)
         disk_size.append(lr9)
-        accreted_mass.append((sink.mass).value_in(units.MJupiter)[0])
-
-        #if model_time.value_in(units.yr) % 50 == 0:
+       
+        # Plotting system
         print 'Time = %.1f yr:'%model_time.value_in(units.yr), \
                   'a = %.2f au, e = %.2f,'%(a, e), \
                   'disk size = %.2f au'%lr9
         plot_map(hydro,Sun_and_Jupiter,'distribution_plot_joined_code_new/{0}.png'.format(int(model_time.value_in(units.yr))),show=False)
+        
         # Evolve the bridge system for one step
         model_time += dt
-        grav_hydro.evolve_model(model_time)#, timestep)
+        grav_hydro.evolve_model(model_time)
         channel_from_grav.copy()
         channel_from_hydro.copy()
 
-        # Add the 'sinked' mass to Jupiter & keep the sink particle along with Jupiter
-        #sink = hydro.particles[0]
+        # Calculating accreted mass in new position
+        sink.position = Jupiter.position
+        sink.radius = Hill_radius(a, e, Sun_Jupiter) | units.AU       
         removed_particles = hydro_sink_particles(sink, disk_gas)
-        #Jupiter = gravity.particles[1]
         Jupiter = gravity.particles[0]        
         Jupiter.mass += sink.mass - sink0_mass
-        sink0_mass = sink.mass
-        sink.position = Jupiter.position
-        sink.radius = a * (1-e) * ((1.0|units.MJupiter).value_in(units.MSun)/3.)**(1./3.) | units.au
+        sink0_mass = sink.mass.copy()
         channel_to_grav.copy()
         channel_to_hydro.copy()
 
@@ -226,7 +209,6 @@ def main(t_end=1000.|units.yr, dt=10.|units.yr):
 
     # Initialising the direct N-body integrator
     gravity = ph4(converter)
-    #gravity.particles.add_particles(Sun_and_Jupiter)
     gravity.particles.add_particle(Jupiter)
     gravity.timestep = dt
     
@@ -247,7 +229,6 @@ def main(t_end=1000.|units.yr, dt=10.|units.yr):
    
     # Initialising the SPH code
     sph = Fi(converter, mode="openmp")
-    #sph.particles.add_particles(sink)
     sph.gas_particles.add_particles(disk_gas)
     sph.dm_particles.add_particle(Sun)
     sph.parameters.timestep = dt
@@ -266,33 +247,6 @@ if __name__ == "__main__":
     
     sim_output = np.column_stack((times.value_in(units.yr), a_Jup, e_Jup, disk_size, accreted_mass))
     np.savetxt('sim_output_q3.csv',sim_output,delimiter=',')
-    
-
-    #N_rho = 1000
-    #L = 2 * Rmax
-    #rho = make_map(sph, N=N_rho, L=L)
-    
-    #fig = plt.figure(figsize=(8, 20))
-    #ax1 = fig.add_subplot(311)
-    #ax2 = fig.add_subplot(312)
-    #ax3 = fig.add_subplot(313)
-    #ax1.plot(times, a_Jup)
-    #ax2.plot(times, e_Jup)
-    #ax3.plot(times, disk_size)
-
-    #for ax in [ax1, ax2, ax3]:
-    #    ax.set_xlabel('Time [yr]')
-    
-    #ax1.set_ylabel('a [au]')
-    #ax2.set_ylabel('e')
-    #ax3.set_ylabel('disk size [au]')
-
-    #plt.show()
-    
-    #plt.imshow(numpy.log10(1.e-5 + rho.value_in(units.amu / units.cm**3)),
-    #              extent=[-L / 2, L / 2, -L / 2, L / 2], vmin=10, vmax=15)
-    #plt.title(t_end)
-    #plt.savefig('test.png')
 
 
 